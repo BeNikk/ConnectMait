be7bfe8442e567e8f1d0a131bbf41fdb17eb3c5c
BeNikk <bhattnik442@gmail.com>
2024-01-25 09:36:25 +0530

    Created Signup signin pages

--- client/connectmait/src/App.tsx
+++ client/connectmait/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+
 function App() {
   return (
     <>
-      <div></div>
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ client/connectmait/src/components/Signup.tsx
@@ -0,0 +1,63 @@
+import { TextField } from "@mui/material";
+import { Button } from "@/components/ui/button";
+
+export default function Signup() {
+  return (
+    <>
+      <div className="flex flex-row h-screen justify-center">
+        <div className="flex flex-col rounded-2xl w-[400px] h-[665px] mt-[5%]  bg-white p-4">
+          <img
+            src="/m-logo.avif"
+            alt="m logo"
+            className="w-[50px] h-[50px] ml-4 mt-[32px]"
+          />
+          <p className="text-md font-semi-bold mt-4 ml-4">
+            Create Your Account
+          </p>
+          <p className="text-[#656565] ml-4 mt-2">
+            to continue to Mait-connect
+          </p>
+          <p className="font-semi-bold text-[#656565] text-md ml-4 mt-8">
+            UserName
+          </p>
+          <div className="ml-4 mt-2 w-[300px] h-[16px]">
+            <TextField
+              id="outlined-basic"
+              label=""
+              variant="outlined"
+              placeholder="Username"
+              className="w-full h-full"
+            />
+          </div>
+          <p className="font-semi-bold text-[#656565] text-md ml-4 mt-16">
+            Email
+          </p>
+          <div className="ml-4 mt-2 w-[300px] h-[16px]">
+            <TextField
+              id="outlined-basic"
+              label=""
+              variant="outlined"
+              placeholder="someone@example.com"
+              className="w-full h-full"
+            />
+          </div>
+          <p className="font-semi-bold mt-16 ml-4 text-[#656565]">Password</p>
+          <div className="ml-4 mt-2 w-[300px] h-[16px]">
+            <TextField
+              id="outlined-basic"
+              label=""
+              variant="outlined"
+              placeholder="password"
+              className="w-full h-full"
+            />
+          </div>
+          <div className="mt-16 ml-24">
+            <Button variant="outline" className="bg-black text-white">
+              Create Account
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
