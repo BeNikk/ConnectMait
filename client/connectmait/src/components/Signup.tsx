c6c9ee69f4977aacd6301c5781a896cc00b3227b
Nikhil <154296996+BeNikk@users.noreply.github.com>
2024-02-02 19:18:38 +0530

    Update Signup.tsx

--- client/connectmait/src/components/Signup.tsx
+++ client/connectmait/src/components/Signup.tsx
@@ -21,7 +21,7 @@ export default function Signup() {
     const data = schema.parse(postData);
 
     try {
-      const response = await axios.post("http://localhost:3000/signup", data);
+      const response = await axios.post("https://connect-mait-nikhils-projects-a5ecaa21.vercel.app/signup", data);
       if (response && response.data) {
         localStorage.setItem("token", response.data.token);
         console.log(response.data);
