2e6f54599b79a9149e070c6491d9227d44c57acc
BeNikk <bhattnik442@gmail.com>
2024-02-03 09:46:39 +0530

    br added

--- client/connectmait/src/components/ProfilePage.tsx
+++ client/connectmait/src/components/ProfilePage.tsx
@@ -30,7 +30,7 @@ export default function ProfilePage() {
     <>
       {userName && (
         <div className="flex flex-row justify-center relative">
-          <div className="absolute bg-[#101117] w-[70vw] h-[40vh] top-24 lg:top-32 lg:w-[40vw] lg:h-[50vh]">
+          <div className="absolute bg-[#101117] w-[90vw] h-[40vh] top-24 lg:top-32 lg:w-[40vw] lg:h-[50vh]">
             <div className="relative ">
               <div className="bg-[#1A8CD8] text-center flex items-center justify-center rounded-full w-16 h-16 absolute left-2 top-2 lg:left-4">
                 <div className="font-bold text-white">
@@ -42,16 +42,15 @@ export default function ProfilePage() {
                 {userName}
               </div>
               <div className="absolute left-4 font-bold text-white top-24 lg:top-32">
-                Email
+                Email <br />
               </div>
-              <br />
+
               <div className="absolute left-20 top-24 lg:top-32 font-bold text-white">
                 {email}
               </div>
               <div className="absolute left-4 font-bold text-white top-32 lg:top-44">
-                About
+                About <br />
               </div>
-              <br />
               <div className="absolute left-20 top-32 lg:top-44 font-bold text-white">
                 {about}
               </div>
