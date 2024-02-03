0c03d16d0cfd5744337c66b1e0275e43001769cc
BeNikk <bhattnik442@gmail.com>
2024-02-03 09:51:06 +0530

    other profile

--- client/connectmait/src/components/OtherProfile.tsx
+++ client/connectmait/src/components/OtherProfile.tsx
@@ -28,7 +28,7 @@ export default function OtherProfile() {
     <>
       {userName && (
         <div className="flex flex-row justify-center relative">
-          <div className="absolute bg-[#101117] w-[70vw] h-[40vh] top-24 lg:top-32 lg:w-[40vw] lg:h-[50vh]">
+          <div className="absolute bg-[#101117] w-[100vw] h-[40vh] top-24 lg:top-32 lg:w-[40vw] lg:h-[50vh]">
             <div className="relative ">
               <div className="bg-[#1A8CD8] text-center flex items-center justify-center rounded-full w-16 h-16 absolute left-2 top-2 lg:left-4">
                 <div className="font-bold text-white">
