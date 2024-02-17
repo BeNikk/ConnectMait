dc3348192e85022b0bf3c33e97c07a582890cedf
BeNikk <bhattnik442@gmail.com>
2024-02-17 17:41:36 +0530

    feat:defined layout for the application

---
+++ maitconnect/app/components/Bottombar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Bottombar = () => {
+  return (
+    <div className="block lg:hidden">
+      <div className="fixed bottom-0 w-full">
+        <p className="text-white font-bold">BottomBar</p>
+      </div>
+    </div>
+  );
+};
+
+export default Bottombar;
---
+++ maitconnect/app/components/Leftsidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Leftsidebar = () => {
+  return (
+    <div className="hidden lg:block">
+      <div className="fixed left-4 h-full">
+        <p className="text-white font-bold ">Leftsidebar</p>
+      </div>
+    </div>
+  );
+};
+
+export default Leftsidebar;
